38a3372da87427375d885c539133d2871b8ae7ee
Franklin2509 <frankdesa2506@gmail.com>
2024-01-15 22:33:47 -0500

    Cambios

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleccionar el elemento
+    var semana1 = document.querySelector('.semana1');
+
+    // Añadir la clase 'fadeInUp' cuando la página esté completamente cargada
+    semana1.classList.add('fadeInUp');
+});
