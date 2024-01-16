fb3fe3750cec96efa308dea3872f132ed43cda61
Franklin2509 <frankdesa2506@gmail.com>
2024-01-15 22:43:48 -0500

    Cambio de git

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Seleccionar el elemento
-    var semana1 = document.querySelector('.semana1');
-
-    // Añadir la clase 'fadeInUp' cuando la página esté completamente cargada
-    semana1.classList.add('fadeInUp');
-});
